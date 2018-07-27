c1ee761b42829cd07d5dc08cb4ae14526ddfc6ed
SutherlandB <41080577+SutherlandB@users.noreply.github.com>
2018-07-27 15:33:53 -0400

    Add files via upload

--- Palace copy/3dFurlo.js
+++ Palace copy/3dFurlo.js
@@ -1,9 +1,11 @@
+let clicked = false;
+
 function setup() {
-    createCanvas(1000, 1000, WEBGL);
-      canvas.parent('sketch-holder');
+    createCanvas(windowWidth, 1000, WEBGL);
 }
 let angle = 10
 function draw() {
+
     background(0);
     normalMaterial();
 
@@ -11,11 +13,13 @@ function draw() {
     noStroke();
     translate(mouseX - width/2, mouseY - height/2);
     rotateY(frameCount * 0.05);
-    rotateX(frameCount * 0.05);
+    rotateX(frameCount * 0.07);
     rotateZ(frameCount * 0.04);
     
     torus(100, 50); 
     //rect(0,0,150,150); 
     angle += 0.07;
-    pop();
+    //pop(); 
+  
 }
+
